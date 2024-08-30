89087bc95a7aca5900f5b2a019603df5df5642e4
rahat-israil <rahat.bin.israil>
2024-08-30 20:03:53 +0600

    Complete Task 2

---
+++ Task2.js
@@ -0,0 +1,15 @@
+// Write a program to calculate the average marks of Mathematics, Biology, Chemistry, Physics, and Bangla of a student.
+
+// Input:
+// The first line of the input is the marks of the five subjects mentioned above, respectively.
+
+// Output:
+// Print the result in 2 decimal places.
+
+
+const fiveSubjectMark = (75.25, 65, 80, 35.45, 99.50);
+const totalMark = 75.25 + 65 + 80 + 35.45 + 99.50 ;
+
+const averageMarks = totalMark / 5;
+
+console.log(averageMarks.toFixed(2));
