fc7a8e1e596cc76f01e5b50007230f5c79554840
rahat-israil <rahat.bin.israil>
2024-08-30 19:43:34 +0600

    Complete Task 1

---
+++ Task1.js
@@ -0,0 +1,18 @@
+// You went to the supermarket to buy some oranges and apples. Calculate how much money the shopkeeper will return.
+
+// Input:
+// The first line of the input is the taka you have. The second line is the cost of 1 kg of oranges and 1 kg of apples.
+
+// Output:
+// Print the result.
+
+
+const iHaveMoney = 1000;
+const oneKgOrangePrice = 150;
+const oneKgApplePrice = 250;
+
+const totalPayAmmount = oneKgOrangePrice + oneKgApplePrice;
+
+const returnMoney = iHaveMoney - totalPayAmmount;
+
+console.log(returnMoney);
